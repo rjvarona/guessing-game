aff490c9822009e16cec00a42e6c37da89c2ceee
RJ Varona <rjvarona16@gmail.com>
2017-08-01 16:28:22 -0400

    Add files via upload

---
+++ GuessingGame.py
@@ -0,0 +1,18 @@
+import random
+value = int(input("Guess a value from 1 to 100:"))
+x = random.randint(1,101)
+if value > x:
+	print("The number is too high")
+elif value < x:
+        print ("The number is too low")
+elif value == x:
+	print("you got it fam")
+while value != x:
+	value = int(input("Guess a value from 1 to 100:"))
+	if value > x:
+		print("The number is too high")
+	elif value < x:
+		print ("The number is too low")
+	elif value == x:
+		print("you got it fam")
+
